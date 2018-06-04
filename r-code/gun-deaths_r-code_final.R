ec00ef8cfaf0bc6697de24e9521a4952d16a4a84
Chris Church <cmchurch@berkeley.edu>
2018-06-04 12:15:13 -0700

    set gun deaths to defaults

--- r-code/gun-deaths_r-code_final.R
+++ r-code/gun-deaths_r-code_final.R
@@ -17,18 +17,15 @@ x_min = 1990                  #minimum year to show on x axis, default = 1990
 x_max = 2015                  #maximum year to show on x axis, default = 2015
 y_min = 0                     #mimumum number of deaths to show on y axis, default is zero
 y_max = 1000                  #maximum number of deaths to show on y axis, default is 1000
-plot_title_size = 10          #how big do you want the size of the title font? , default is 10
-plot_subtitle_size = 10       #how big do you want the size of the subtitle font?, default is 10
+plot_title_size = 32          #how big do you want the size of the title font? , default is 32
+plot_subtitle_size = 22       #how big do you want the size of the subtitle font?, default is 22
 caption_size=10               #how big do you want the size of the caption font?, default is 10
-text_size = 10                #how big do you want the size of the text (including axis values)?, default is 10
-point_size = 1                #how big do you want the size of the size of the points?, default is 1
-line_width = 1                #how big do you want the size of the line width?, default is 1
 #VARIABLES FOR GRAPH LABELS
-plot_title = "Title"  #title of the Plot
-plot_subtitle = "Subtitle"  #subtitle of plot
-plot_caption = "Caption"
-point_label = "Point Label"
-x_label = "X Label"
+plot_title = "Gun Deaths in Florida"  #title of the Plot
+plot_subtitle = "Number of murders committed using firearms"  #subtitle of plot
+plot_caption = "SOURCE: Florida Department of Law Enforcement"
+point_label = "2005\nFlorida enacted\nits 'Stand Your\nGround' law"
+x_label = ""
 y_label = ""
 #-----------------------------------------------------------------------------------------------
 #DATA FROM FLORIDA DEPARTMENT OF LAW ENFORCEMENT
@@ -48,8 +45,8 @@ g = g + geom_bar(stat="identity", fill=fill_color, alpha=fill_transparency)
 } else if (plottype=="scatter") {
 #SCATTERPLOT
 if (draw_scatter_fill==T) {g = g + geom_ribbon(aes(ymin = y_min,ymax=DEATHS), fill=fill_color,alpha=fill_transparency)}
-if (draw_scatter_line==T) {g = g + geom_line(size=line_width) }
-if (draw_scatter_points==T) { g = g + geom_point(size=point_size) } 
+if (draw_scatter_line==T) {g = g + geom_line(size=2) }
+if (draw_scatter_points==T) { g = g + geom_point(size=5) } 
 } 
 #SCALE 
 g = g + coord_cartesian(ylim = c(y_min, y_max))
@@ -64,7 +61,7 @@ if(label_stand_your_ground==T) {
   g = g + geom_segment(aes(x = 2005, y = deaths_syg-150, xend=2005, yend=deaths_syg-20),color="white",size=1)
 }
 #COLOR AND THEME
-g = g + theme_bw() + theme(plot.title = element_text(size=plot_title_size),plot.subtitle = element_text(size=plot_subtitle_size),text = element_text(size=text_size),plot.caption=element_text(size=caption_size),axis.text.x = element_text(angle = 45, hjust = 1))
+g = g + theme_bw() + theme(plot.title = element_text(size=plot_title_size),plot.subtitle = element_text(size=plot_subtitle_size),text = element_text(size=20),plot.caption=element_text(size=caption_size),axis.text.x = element_text(angle = 45, hjust = 1))
 g = g + theme( panel.grid.major.x = element_blank(),panel.grid.minor.x = element_blank(),panel.grid.major.y = element_line( size=.1, color="black" ))
 #COORD FLIP CHECK
 if(inverse_coords==T) {g = g + coord_flip()}
